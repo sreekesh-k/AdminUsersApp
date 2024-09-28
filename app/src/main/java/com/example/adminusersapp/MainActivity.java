15ed01c2ea5ce22cc75091b2cf44c5a261cd1fab
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-09-28 12:02:49 +0530

    Main Activity

--- app/src/main/java/com/example/adminusersapp/MainActivity.java
+++ app/src/main/java/com/example/adminusersapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.adminusersapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +12,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login,register,admin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +23,29 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        login = findViewById(R.id.loginbtn);
+        register = findViewById(R.id.registerbtn);
+        admin = findViewById(R.id.adminbtn);
+
+        login.setOnClickListener(v -> {
+            //from where to where
+            Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+            startActivity(intent);
+        });
+
+        register.setOnClickListener(v -> {
+            //from where to where
+            Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        admin.setOnClickListener(v -> {
+            //from where to where
+            Intent intent = new Intent(MainActivity.this,AdminActivity.class);
+            startActivity(intent);
+        });
+
+
     }
 }
